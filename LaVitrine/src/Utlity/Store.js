bb48bb8d772998762e617849427f96201cfe3122
SAURABH SINGH <164341224+testingbysaurabh@users.noreply.github.com>
2025-08-31 03:17:58 +0530

    Delete LaVitrine directory

--- LaVitrine/src/Utlity/Store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartSliceReducer from "./CartSlice"
-
-const store = configureStore({
-  reducer: {
-    cart: cartSliceReducer,
-  },
-});
-export default store;
